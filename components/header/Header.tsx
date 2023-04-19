aa8291fe4ded8c089c00c7098403af1e8bfc7bfc
Andriamahay11master <48830551+Andriamahay11master@users.noreply.github.com>
2023-04-19 09:24:46 +0300

    Add structure header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,5 +2,31 @@ import * as React from 'react';
 import './header.scss';
 
 export default function Header() {
-  return <p>test</p>;
+  return (
+    <header>
+      <div className="headerLeft">
+        <a href="">
+          <img src="" alt="" />
+        </a>
+      </div>
+      <div className="headerRight">
+        <ul>
+          <li>
+            <a href="">Product</a>
+          </li>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="" className="btn btnlog">
+              Log in
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
