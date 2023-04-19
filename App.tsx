44c9f0abd546b0f74678c8839b994daf28a7221b
Andriamahay11master <48830551+Andriamahay11master@users.noreply.github.com>
2023-04-19 14:21:07 +0300

    correction font

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Header from './components/header/Header';
+import Header from './src/components/header/Header';
 import './App.scss';
 
 export default function App() {
