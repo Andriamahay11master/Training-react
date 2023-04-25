6dd15ae97757430c5a4495921b6221ef1c8029ce
Andriamahay11master <48830551+Andriamahay11master@users.noreply.github.com>
2023-04-25 14:22:01 +0300

    add component list Article

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Header from './src/components/header/Header';
 import Footer from './src/components/footer/Footer';
 import Banner from './src/components/banner/Banner';
+import Listarticle from './src/components/listArticle/Listarticle';
 import './App.scss';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <div>
       <Header />
       <Banner />
+      <Listarticle />
       <Footer />
     </div>
   );
---
+++ src/components/listArticle/Listarticle.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export default function Listarticle(){
+  return (
+    <div className="listBlock">
+      <div className="listBlockBody">
+        <h2 className="listBlockTitle">Latest news</h2>
+        <div className="listArticleContent">
+
+        </div>
+      </div>
+      
+    </div> 
+  );
+}
